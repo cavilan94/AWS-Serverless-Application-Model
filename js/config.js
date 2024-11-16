e5f71c508fe21ac7c73d539d033276840cb90947
cavilan94 <camiloavilan94@gmail.com>
2024-11-16 09:55:02 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-1_mgOPJ7MuF
+        userPoolClientId: '', // e.g. 6lgl9ujmrgk5d6cr3326ejtiri
         region: '' // e.g. us-east-2
     },
     api: {
