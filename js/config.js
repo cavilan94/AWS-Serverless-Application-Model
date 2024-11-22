643594876ce284f121942d4f45ec87c19c57baae
cavilan94 <camiloavilan94@gmail.com>
2024-11-22 17:19:46 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-1_mgOPJ7MuF
         userPoolClientId: '', // e.g. 6lgl9ujmrgk5d6cr3326ejtiri
-        region: '' // e.g. us-east-2
+        region: '' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
