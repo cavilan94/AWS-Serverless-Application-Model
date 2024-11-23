b7cdfe06ee5c915ab5386d01f5bc5e4b7854f8c8
cavilan94 <camiloavilan94@gmail.com>
2024-11-22 20:09:57 -0500

    new_configuration

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_mgOPJ7MuF
-        userPoolClientId: '', // e.g. 6lgl9ujmrgk5d6cr3326ejtiri
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_mgOPJ7MuF', // e.g. us-east-1_mgOPJ7MuF
+        userPoolClientId: '6lgl9ujmrgk5d6cr3326ejtiri', // e.g. 6lgl9ujmrgk5d6cr3326ejtiri
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: 'https://pwuimcpg2g.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://pwuimcpg2g.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
